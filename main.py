771cd9c0f3757309a001dd7f8fcbc576ad06923b
aimabe <aimen.aberra@gmail.com>
2024-09-06 22:04:14 -0400

    report complete

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    #print(number_of_words(file_contents))
-    #number_of_characters(file_contents)
+
     report(file_contents)
 
 def number_of_words(input_text):
@@ -12,7 +11,7 @@ def number_of_words(input_text):
 def number_of_characters(input_string):
     number_of_char = {}
     lowered_string = input_string.lower()
-    #print(lowered_string)
+
     for s in lowered_string:
         if s in number_of_char:
             number_of_char[s] += 1
@@ -24,21 +23,23 @@ def report(text_file):
     word_count = number_of_words(text_file)
     num_of_chars = number_of_characters(text_file)
     list_of_dict = []
-    
-    #def sort_on(dict):
-        #for item in dict:
-            #print(item)
-        #return dict
 
     for key in num_of_chars:
         if key.isalpha():
             list_of_dict.append({key: num_of_chars[key]})
-   
+    #sort dictionary, descending by value
     sorted_list = sorted(list_of_dict, key=lambda x: list(x.values())[0], reverse=True)
-    #print(list_of_dict.sort(reverse=True, key=lambda x: list(x.keys())[0]))
-        
     
-    print(sorted_list)
+    #Print report
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print()
+    
+    for item in sorted_list:
+        for key in item.keys():
+            print(f"The '{key}' character was found {item[key]} times")
+    
+    print("--- End report ---")
     
         
 main()
